129c785f27698b052d8a132d90a0ce2c26208004
Pankaj Goyal <pkjg1807@gmail.com>
2019-06-22 12:27:27 +0530

    validation checked

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './Components/Header.js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      image_url:'',
+      upload:false,
+      file:[],
+      name:''
+    }
+  }
+
+  onImageSelect = (e)=>{
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        image_url: reader.result,
+      });
+    }
+
+    reader.readAsDataURL(file)
+  }
+
+  onClickUpload = () =>{
+    if(this.state.image_url && this.state.name)
+    this.setState({uploading:true});
+  }
+
+  onNameChange = (event) =>{
+    this.setState({name:event.target.value})
+  }
+
+  onRemove = () =>{
+    this.setState({uploading:false})
+  }
+  render(){
+    //console.log(this.state.image_url);
+    return (
+      <div className="App">
+        <Header />
+        <div className="body">
+          <div className="inner">
+            <div className="name">
+              Name :-
+              <input className="input_name" type="text" placeholder="Your Name" value={this.state.name} onChange={this.onNameChange} required={true} ></input>
+            </div>
+            <div className="url">
+              Select Profile Picture...<input className="input_url" type="file"  onChange={this.onImageSelect} />
+            </div>
+            <div className="button_submit">
+              <button className="submit" onClick={this.onClickUpload} >Submit </button>
+            </div>
+            {
+              this.state.uploading === true
+              ? <div className="onsubmit" >
+                  <span className="submitname">
+                      Welcome Back..... {this.state.name}
+                  </span>
+                  <img src={this.state.image_url} alt="" />
+                  <button className="submit" onClick={this.onRemove} >Reset</button>
+                </div>
+              :<div >
+                Enter Details...
+              </div>
+            }
+
+          </div>
+          <footer className="footer">
+          </footer>
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
